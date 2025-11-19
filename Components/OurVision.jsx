e34a2d7ba5c4cacfbb8a732371af3a37bd5e6ce4
abdullah <smabdulla958>
2025-11-20 02:10:26 +0500

    add a contact us section

--- Components/OurVision.jsx
+++ Components/OurVision.jsx
@@ -8,7 +8,7 @@ const OurVision = () => {
       
     <div className='grid grid-cols-1 md:grid-cols-2 items-center justify-between  2xl:py-14  sm:px-16 px-10   md:pt-0 text-[#ece3e3] text-xl '>
 
-            <Image src="/Mission.png" alt="mission" width={500} height={500} lazy="loading" className='rounded-2xl order-2 md:order-1 mt-5 md:mt-0 ' />
+            <Image src="/Mission.png" alt="mission" width={400} height={400} lazy="loading" className='rounded-2xl order-2 md:order-1 mt-5 md:mt-0 ' />
       
         <h1 className='text-sm sm:text-xl lg:text-2xl font-bold  md:ml-10  justify-start text-justify order-1 md:order-2'>create value,share Knowledge and empower businesses and learners through the power of technology</h1>
 
