225d14684fd08a49bec372fddfa372d72024958d
abdullah <smabdulla958>
2025-11-22 02:13:04 +0500

    remove loader

--- app/page.js
+++ app/page.js
@@ -1,4 +1,3 @@
-
 import HomePage from "@/Components/HomePage"
 import About from "@/Components/About";
 import Skills from "@/Components/Skills";
@@ -6,8 +5,7 @@ import Services from "@/Components/Services";
 import Project from "@/Components/Project";
 import ContactUs from "@/Components/ContactUs";
 import OurVision from "@/Components/OurVision";
-export default async function Home() {
-  await new Promise(resolve => setTimeout(resolve, 200));
+export default function Home() {
   
   return (
     <div className="bg-gray-900">
